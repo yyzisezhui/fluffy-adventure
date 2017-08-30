464226606aadd4acc65c9401ed5c3fa90cb4f5a9
yyzisezhui <yuanyi@yulintu.cn>
2017-08-30 20:54:22 +0800

    再更新

---
+++ untitled/src/main/java/com/yy/com/A2.java
@@ -0,0 +1,13 @@
+package com.yy.com;
+
+/**
+ * Created by Administrator on 2017/8/30.
+ */
+public class A2 {
+    private static String name;
+
+    public static void main(String[] args) {
+        System.out.println(name);
+    }
+}
+
---
+++ untitled/src/main/java/com/yy/com/cbzb/H.java
@@ -0,0 +1,8 @@
+package com.yy.com.cbzb;
+
+/**
+ * Created by Administrator on 2017/8/30.
+ */
+public class H {
+
+}
